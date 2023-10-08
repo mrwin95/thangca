18b0a1ec773073b60943b199f138d30fa833ce0d
Thang Nguyen <mrwin95@gmail.com>
2023-10-08 02:27:49 -0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <nav class="bg-blue-500 p-4 shadow-lg flex flex-col lg:flex-row">
+    <router-link
+      to="/"
+      class="text-while px-4 py-2 m-1 rounded-md"
+      exact-active-class="bg-red-300"
+      >Home</router-link
+    >
+    <router-link
+      to="/about"
+      class="text-while px-4 py-2 m-1 rounded-md"
+      exact-active-class="bg-red-300"
+      >About</router-link
+    >
   </nav>
   <router-view />
 </template>
@@ -13,18 +23,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <h1 class="text-6xl m-4"><span class="text-red-500">Thang</span> Nguyen</h1>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 @Options({
-  components: {
-    HelloWorld,
-  },
+  components: {},
 })
 export default class HomeView extends Vue {}
 </script>
